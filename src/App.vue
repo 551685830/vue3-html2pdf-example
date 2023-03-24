e034e2587c7da4c429ae786c4a2eff3809f193d4
tangxin <551685830@qq.com>
2023-03-24 16:49:36 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <vue3-simple-html2pdf
+    ref="vue3SimpleHtml2pdf"
+    :options="pdfOptions"
+    :filename="exportFilename"
+  >
+    <div class="main-content">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+      <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+      <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+      <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+    </div>
+  </vue3-simple-html2pdf>
+
+  <button @click="download">Download pdf</button>
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+  data() {
+    return {
+      pdfOptions: {
+        margin: 15,
+        image: {
+          type: "jpeg",
+          quality: 1,
+        },
+        html2canvas: { scale: 3 },
+        jsPDF: {
+          unit: "mm",
+          format: "a4",
+          orientation: "p",
+        },
+      },
+      exportFilename: "my-custom-file.pdf",
+    };
+  },
+  methods: {
+    download() {
+      this.$refs.vue3SimpleHtml2pdf.download();
+    },
+  },
+};
+</script>
+
+<style>
+#app,
+.main-content {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import Vue3SimpleHtml2pdf from "vue3-simple-html2pdf";
+
+createApp(App).use(Vue3SimpleHtml2pdf).mount("#app");
